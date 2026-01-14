259aad75d31cd41eb6bded5e34ee6732f113a981
EnWaffel <leoarbeit@outlook.de>
2026-01-14 12:12:31 +0100

    a

--- src/WIFIAPI.cpp
+++ src/WIFIAPI.cpp
@@ -31,7 +31,7 @@ std::vector<std::string> WIFIAPI::GetAvailableNetworks() {
         ls >> bssid >> freq >> signal >> flags >> ssid;
         std::getline(ls, line);
         ssid.erase(ssid.begin(), std::find_if(ssid.begin(), ssid.end(), [](unsigned char c){
-            return !std::isspace(c);
+            return !std::isspace(c); 
         })); 
 
         networks.push_back(ssid);
